fe6635e044ad5df1c8874b7ad0b1dcf4d02bed69
Storm <storm.randolph@gmail.com>
2024-04-19 16:41:31 -0700

    drawing board

--- tictactoe.py
+++ tictactoe.py
@@ -2,13 +2,27 @@ import pygame
 
 #setup
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
+height = 800
+width = 800
+screen = pygame.display.set_mode((width, height))
 running = True
+WHITE = (255,255,255)
 
 while running:
-     for event in pygame.event.get():
+      
+   #drawing board test
+    
+        
+    pygame.draw.line(screen, WHITE, [width / 3, 0], [width / 3, height])
+    pygame.draw.line(screen, WHITE, [width / 3 * 2, 0], [width / 3 * 2, height])
+    
+    pygame.draw.line(screen, WHITE, [0, height / 3], [width, height / 3])
+    pygame.draw.line(screen, WHITE, [0, height / 3 * 2], [width, height / 3 * 2])
+
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+            
+    pygame.display.update()
 
 pygame.quit()
