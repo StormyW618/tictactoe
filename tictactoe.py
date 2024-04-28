db669d288b484af54f0f7c989b8cb59ec9e242bf
Storm <storm.randolph@gmail.com>
2024-04-28 16:36:10 -0700

    fixed algorithm maybe

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm_notebook as tqdm
 
 
 def get_possible_moves(board, player): 
@@ -40,7 +41,97 @@ def get_score(board, depth=0):
         return None
     
 
-    #alpha beta pruning :(
-    def find_value(board, player, alpha=float('-inf'),beta=float('inf'), depth=0 ):
-        return 0
+#alpha beta pruning :(
+def find_value(board, player, alpha=float('-inf'),beta=float('inf'), depth=0 ):
+    score = get_score(board)
+    #return value at last node or score if game is finished
+    if score is not None:
+        return score
+    # maximizing player
+    if player == "max":
+        maxvalue = float('-inf')
+        #iterate over successors
+        successors  = get_possible_moves(board, "max")
+        for i in successors:
+            maxvalue = max(maxvalue,find_value(i, "min", alpha, beta, depth+1))
+            if maxvalue >= beta:
+                return maxvalue
+            alpha = max(alpha, maxvalue)
+        return maxvalue
+    #minimizing player
+    else:
+        minvalue = float('inf')
+        succes  = get_possible_moves(board, "min")
+        for h in succes:
+            minvalue = min(minvalue,find_value(h, "max", alpha, beta, depth + 1))
+            if minvalue <= alpha:
+                return minvalue
+            beta = min(beta, minvalue)
+        return minvalue
+
+
+def find_best_move(board, player):
+    print("Deciding best move...")
+    boards = get_possible_moves(board, player)
+    for i in boards:
+        print(i)
+    values = [find_value(board,
+                        ('max' if player is 'min'
+                        else 'min'))
+            for board in tqdm(boards)]
+    print(values)
+    if None in values:
+        raise ValueError('find_value should always return an integer.')
+    if player is "max":
+        policy_vector = np.array([1 if value == np.amax(values) else 0
+                                for value in values])
+    else:
+        policy_vector = np.array([1 if value == np.amin(values) else 0
+                                for value in values])
+    # Normalize Probabilities
+    policy_vector = policy_vector / np.count_nonzero(policy_vector)
+    return boards[np.random.choice(np.arange(len(values)),
+                                1,
+                                p=policy_vector)[0]]
     
+
+# Plays a match between  a human and the bot, asking the human for their move  via console
+# To select the  role of the  bot (max or min), edit  line 11
+
+def run_demo():
+    board = np.array([[' ', ' ', ' '],
+                        [' ', ' ', ' '],
+                        [' ', ' ', ' ']])
+    score = get_score(board)
+    player = "min"
+    while score is None:
+        if player == "max":   # Set this to max if you want the bot to go first, and min otherwise
+            board = find_best_move(board, player)
+        else:
+            move_entered = False
+            while not move_entered:
+                try:
+                    move = int(input('Choose a move...')) - 1
+                    if not 0 <= move <= 8:
+                        print("Enter an integer between 1 and 9.\n")
+                        continue
+                    elif not board[move//3][move%3] == ' ':
+                        print("That spot is already taken.\n")
+                        continue
+                    else:
+                        board[move//3][move%3]= 'O' if player == "min" else "X"
+                        move_entered = True
+                except ValueError:
+                    print("Enter an integer.\n")
+        score = get_score(board)
+        player = "min" if player == "max" else "max"
+        print(board)
+    if (score == 0):
+        print("Draw")
+    elif (score > 0):
+        print("You Lose")
+    else:
+        print("You Win")
+
+
+run_demo()
